89e4c235ab81bef2601c88c1538661ab475266a4
catOWO2011 <catowo2011@gmail.com>
2024-06-07 22:10:17 -0400

    First commit

---
+++ src/main/java/com/example/student_system_be/StudentSystemBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.student_system_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSystemBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentSystemBeApplication.class, args);
+	}
+
+}
